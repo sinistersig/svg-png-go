f1e8ade2a9000cc8383165c8c96c59d47c19e19b
sinistersig <sigismondmc@gmail.com>
2015-09-14 14:29:46 -0500

    Read svg and print struct

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
 import "fmt"
+import "os"
+import "github.com/rogpeppe/misc/svg"
+import "image"
 
 func example() (val string, err error){
 	val = "test string";
@@ -9,5 +12,14 @@ func example() (val string, err error){
 }
 
 func main() {
-	fmt.Println("works");
+
+	file, err := os.Open("img/test.svg");
+
+	size := image.Point{300,300};
+
+	if err != nil {
+		panic(err);
+	}
+
+	fmt.Println(svg.Render(file, size));
 }
