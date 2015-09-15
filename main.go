d4b1c9b43c1fe945159a64e80f6380d4281fe8c6
sinistersig <sigismondmc@gmail.com>
2015-09-15 08:29:21 -0500

    save image data to png

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"os"
 	"image"
 	"github.com/rogpeppe/misc/svg"
+	"github.com/llgcode/draw2d/draw2dimg"
 )
 
 func example() (val string, err error){
@@ -17,11 +18,22 @@ func main() {
 
 	file, err := os.Open("img/test.svg");
 
-	size := image.Point{300,300};
+	size := image.Point{1000,1000};
 
 	if err != nil {
 		panic(err);
 	}
 
-	fmt.Println(svg.Render(file, size));
+
+	dest, err := svg.Render(file, size);
+
+	if err != nil {
+		panic(err);
+	}
+
+	draw2dimg.SaveToPngFile("/home/test.png", dest);
+
+	file.Close();
+
+	fmt.Println("done");
 }
